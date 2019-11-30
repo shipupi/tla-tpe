2bb37d15432aec3d18eb67252354f3fc8c4b44b2
Jbaader <jbaader@itba.edu.ar>
2019-11-30 18:29:30 -0300

    visual modifications

--- planetsOld.c
+++ planetsOld.c
@@ -1,11 +1,9 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <math.h>
-#include <graphics.h>
-
-
-
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<math.h>
+#include<graphics.h>
+ 
 typedef struct{
 	double x,y,z;
 }vector;
@@ -56,8 +54,6 @@ void initiateSystem(char* fileName){
 		fscanf(fp,"%lf",&masses[i]);
 		fscanf(fp,"%lf%lf%lf",&positions[i].x,&positions[i].y,&positions[i].z);
 		fscanf(fp,"%lf%lf%lf",&velocities[i].x,&velocities[i].y,&velocities[i].z);
-		fscanf(fp,"%lf%lf%lf",&velocities[i].x,&velocities[i].y,&velocities[i].z); 
-
 	}
  
 	fclose(fp);
@@ -118,13 +114,7 @@ int main(int argC,char* argV[])
 	int i,j;
 	int visual = true;
 	int gd = DETECT, gm; 
-	
-	//planet settings
-	int radius[5]={3,3,3,3,3};    						//tamaño
-	int color[5]={YELLOW,BLUE,RED,4,5};    					//del 0(negro) al 16
-	char *name[5]={"1","2","3","4","5"}; 				//nombres de los planetas
-	bool visibility[5]={true,true,true,true,true};		//visibilidad
-
+	char *name[3]={"1","2","3"};
 
 	signal(SIGTERM, sigterm_handler);
   	signal(SIGINT, sigterm_handler);
@@ -136,73 +126,28 @@ int main(int argC,char* argV[])
 		fprintf(stderr, "Usage : %s <file name containing system configuration data>\n",argV[0]);
 	else{
 		initiateSystem(argV[1]);
-		int xoffset = getmaxx() / 2;
-		int yoffset = getmaxy() / 2;
-		int scale = 1;
 		if (!visual) {
 			fprintf(stderr, "Body   :     x              y               z           |           vx              vy              vz   ");
 		}
 		for(i=0;i<timeSteps;i++){
 			fprintf(stderr, "\nCycle %d\n",i+1);
-			
-			//borro de la pantalla
-			//for(j=0;j<bodies;j++){
-			//	if (visual)  {
-				/* code */
-			//		setcolor(0);
-			//		if(visibility[j]){
-			//			//outtextxy((positions[j].x*scale+xoffset+5),(positions[j].y*scale+yoffset+5),name[j]);
-			//			setcolor(0);
-			//			bar((positions[j].x*scale+xoffset+5),(positions[j].y*scale+yoffset),(positions[j].x*scale+xoffset+5)+strlen(name[j])*20, (positions[j].y*scale+yoffset)+20);
-			//			setcolor(0);
-			//			fillellipse((positions[j].x*scale+xoffset),(positions[j].y*scale+yoffset), radius[j], radius[j]);
-			//			//circle((positions[j].x*scale+xoffset),(positions[j].y*scale+yoffset),radius[j]);
-			//			//floodfill((positions[j].x*scale+xoffset),(positions[j].y*scale+yoffset),color[j]);
-			//		}
-			//	}				
-			//	fprintf(stderr, "Body %d : %lf\t%f\t%lf\t|\t%lf\t%lf\t%lf\n",j+1,positions[j].x,positions[j].y,positions[j].z,velocities[j].x,velocities[j].y,velocities[j].z);
-			//}
-
-			simulate();
-
-			if (i == 0) {
-				int xm = 0;
-				int ym = 0;
-				for(j=0;j<bodies;j++){
-					if (positions[j].x > xm) {
-						xm = positions[j].x;
-					}
-					if (positions[j].y > ym) {
-						ym = positions[j].y;
-					}
-				}
-				int determining_value = MAX(xm,ym);
-				if (determining_value == xm) {
-					scale = (0.8 * getmaxx() / 2) / xm;
-				} else {
-					scale = (0.8 * getmaxy() / 2) / ym;
-				}
-			}
 			cleardevice();
+			simulate();
+			setcolor(WHITE);
 			for(j=0;j<bodies;j++){
-				if (visual && (positions[j].x*scale+xoffset)<getmaxx() && (positions[j].x*scale+xoffset)>=0 && (positions[j].y*scale+yoffset)<getmaxy() && (positions[j].y*scale+yoffset) >=0 )  {
-							/* code */
-					setcolor(color[j]);
-					if(visibility[j]){
-						outtextxy((positions[j].x*scale+xoffset+radius[j]+5),(positions[j].y*scale+yoffset+radius[j]+3),name[j]);
-						fillellipse((positions[j].x*scale+xoffset),(positions[j].y*scale+yoffset), radius[j], radius[j]);
-						//circle((positions[j].x*scale+xoffset),(positions[j].y*scale+yoffset),radius[j]);
-						//floodfill((positions[j].x*scale+xoffset),(positions[j].y*scale+yoffset),color[j]);
+				if (visual)  {
+									/* code */
+					if(j>0){
+						setcolor(j+1);
 					}
-				}else{
-					fprintf(stderr, "OUT OF BOUNDS");
-
-				}			
+					outtextxy((positions[j].x+20)*10,(positions[j].y+20)*10,name[j]);
+					circle((positions[j].x+20)*10,(positions[j].y+20)*10,2);
+					
+				}				
 				fprintf(stderr, "Body %d : %lf\t%f\t%lf\t|\t%lf\t%lf\t%lf\n",j+1,positions[j].x,positions[j].y,positions[j].z,velocities[j].x,velocities[j].y,velocities[j].z);
-
 			}
 			if (visual)  {
-				delay(3);
+				delay(10);
 			}
 
 		}
